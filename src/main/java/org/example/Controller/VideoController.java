28102cb9424900dd0ccc2a9243bbf02dc9bf3463
mronion112 <mr.onion112@gmail.com>
2025-11-28 01:31:22 +0700

    Add new feature scrapping web & json write/read

--- src/main/java/org/example/Controller/VideoController.java
+++ src/main/java/org/example/Controller/VideoController.java
@@ -1,15 +1,14 @@
 package org.example.Controller;
 
+import org.example.Module.Entity.Data.Video.LocalVideoEntity;
 import org.example.Module.Entity.Data.Video.WebVideoEntity;
-import org.example.Module.Repsitory.Data.LocalDataRespository;
 import org.example.Module.Repsitory.Data.Video.LocalVideoRepository;
 import org.example.Module.Repsitory.Data.Video.WebVideoRepository;
-import org.example.Module.Repsitory.Data.WebDataRespository;
-import org.example.Service.VideoService;
 
+import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
+
 
 public class VideoController {
 
@@ -18,15 +17,32 @@ public class VideoController {
     }
 
     public  HashMap<String, WebVideoEntity> listVideoWeb = new HashMap<>();
-    public  LocalDataRespository localDataRespository = new LocalDataRespository();
-    public  WebDataRespository webDataRespository = new WebVideoRepository();
+//    public  LocalDataRespository localDataRespository = new LocalDataRespository();
+//    public  WebDataRespository webDataRespository = new WebVideoRepository();
 
-
-    public  WebVideoRepository webVideoRepository = new WebVideoRepository();
+    public String StorageFolderDir;
+    public  WebVideoRepository webVideoRepository ;
     public  LocalVideoRepository localVideoRepository = new LocalVideoRepository();
 
-    public VideoController() {}
+    public int id = 1;
 
+    public VideoController(String StorageFolderDir) {
+        this.StorageFolderDir = StorageFolderDir;
+        this.webVideoRepository = new WebVideoRepository(new File(StorageFolderDir));
+    }
+
+
+    public WebVideoEntity downLoadVideoFromName(String nameVideo, HashMap<String, WebVideoEntity> localVideoEntities, HashMap<String, WebVideoEntity> processJsonData, int idFolder) {
+        try {
+            return webVideoRepository.downloadData(nameVideo, localVideoEntities, processJsonData, idFolder);
+        } catch (IOException e) {
+            System.out.println("Can't connect to the server");
+        } catch (InterruptedException e) {
+            System.out.println("Download Error : InterruptedException");
+        }
+
+        return null;
+    }
 
 
     public HashMap<String, WebVideoEntity> scrappingVideoChannel(String urlChannel){
@@ -35,10 +51,30 @@ public class VideoController {
 
     }
 
+    public void writeToProcessJson(File processJson, HashMap<String, WebVideoEntity> listVideoDownloaded) {
+        try {
+            webVideoRepository.wirteProcessJson(processJson, listVideoDownloaded);
+        }catch (IOException e){
+            System.out.println("Error to write file processJson");
+        }
+    }
+
 
     public void removeVideoFormList(String nameVideo) throws IOException {
         webVideoRepository.deleteWebVideoByName(nameVideo);
     }
+
+    public void scanDataVideoScrap(String dataVideoSrapDir){
+
+    }
+
+    public void downloadVideoToLocal(String ouPutFolder, String folderId){
+
+
+    }
+
+
+
 
 
 
